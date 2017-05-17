e15fc5e837bfaa4ca85f886b686194914bbcc11d
nyarudea <adenuar11@gmail.com>
2017-05-17 12:41:29 +0900

    signup

---
+++ signup.php
@@ -0,0 +1,17 @@
+<?php 
+$name=$_POST['Name'];
+$email=$_POST['Email'];
+$password=$_POST['Password'];
+$cpassword=$_POST['Password2'];
+
+if($password==$cpassword)
+{
+
+}
+else
+{
+	echo"password tdk sesuai<br>";
+	echo"<a href=index.html>bek</a?";
+}
+
+?>
