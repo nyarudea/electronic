9d2d9c2054e8e351027070417de35bed047dc392
nyarudea <adenuar11@gmail.com>
2017-05-17 13:30:29 +0900

    add query to db

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php 
+require 'db/konek.php';
 $name=$_POST['Name'];
 $email=$_POST['Email'];
 $password=$_POST['Password'];
@@ -6,12 +7,16 @@ $cpassword=$_POST['Password2'];
 
 if($password==$cpassword)
 {
-
+$sql = "INSERT INTO member (Username, Email, Password) VALUES ('$name','$email', '$password')";
+                     if ($conn->query($sql) === TRUE) 
+                      {
+                       echo "Sign up Berhasil<br>";
+                       } 
 }
 else
 {
 	echo"password tdk sesuai<br>";
-	echo"<a href=index.html>bek</a?";
 }
+	echo"<a href=index.html>bek</a?";
 
 ?>
