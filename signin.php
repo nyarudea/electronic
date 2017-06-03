e70b94d14cd20bab0e02979d8e1d15db2dce03ce
nyarudea <adenuar11@gmail.com>
2017-06-03 09:19:06 +0900

    if login gagal

--- signin.php
+++ signin.php
@@ -1,10 +1,106 @@
 <?php
 require "db/konek.php";
 session_start();
+?>
+<head>
+<title>Electronic Store a Ecommerce Online Shopping Category Bootstrap Responsive Website Template | Products :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Electronic Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+	SmartPhone Compatible web template, free web designs for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+	function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<!-- Custom Theme files -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/fasthover.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<!-- //js -->
+<!-- web fonts -->
+<link href='//fonts.googleapis.com/css?family=Glegoo:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- //web fonts -->
+<!-- for bootstrap working -->
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+<!-- //for bootstrap working -->
+<!-- start-smooth-scrolling -->
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- //end-smooth-scrolling -->
+</head>
+<body>
 
+	<!-- header -->
+	<div class="header" id="home1">
+		<div class="container">
+			<div class="w3l_logo">
+				<h1><a href="index.php">Electronic Store<span>Your stores. Your place.</span></a></h1>
+			</div>
+			<div class="search">
+				<input class="search_box" type="checkbox" id="search_box">
+				<label class="icon-search" for="search_box"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></label>
+				<div class="search_form">
+					<form action="#" method="post">
+						<input type="text" name="Search" placeholder="Search...">
+						<input type="submit" value="Send">
+					</form>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<!-- //header -->
+	<!-- navigation -->
+		<div class="navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header nav_2">
+					<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+					<ul class="nav navbar-nav">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="products.php">Products </a></li>
+						<li><a href="products1.php">Products1 </a></li>
+						<li><a href="products2.php">Products2 </a></li>
+						<li><a href="about.php">About Us</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- //navigation -->
+	<!-- banner -->
+	<div class="banner banner1">
+		<div class="container">
+			<h2>Great Offers on <span>Mobiles</span> Flat <i>35% Discount</i></h2>
+		</div>
+	</div>
+
+	<?PHP
 $email=$_POST['Email'];
 $password=$_POST['Password'];
-// echo "$email<br>$password";
+//echo "$email<br>$password";
 $sql = $conn->prepare("Select * FROM member WHERE Email=? AND Password=?");
 $sql->bind_param('ss', $email, $password);
 
@@ -15,12 +111,18 @@ if($result->num_rows>0)
 {
 	while($row = $result->fetch_assoc())
 	{
-		if($email==$row['Email'])
+		if($email==$row['Email'] && $password==$row['Password'])
 		{
 			$_SESSION['email'] = $email;
 			header("Location: home.php");
 			exit();
 		}
+		
   }
 }
+else
+		{
+			echo "<div class='container' align=center><h1>Login gagal</h1><br><h2><a href=index.php>  
+			<img src='images/back.png' style=;width:200px;height:100px;border:0'></a></h2></div>";
+		}
 ?>
